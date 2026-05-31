60d7d17842194c04ef4c37d80a70114530e59f62
Krishvika <krishvikadilbagi@gmail.com>
2026-05-31 15:14:53 +0530

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int l=0;
+        int r=0;
+        while (r<n){
+            if (nums[r]!=0){
+                swap(nums[r],nums[l]);
+                l++;
+            }
+            r++;
+        }
+    }
+};
